0c3f2921e11f7ac5be8c6aa5bf40120ae5bcbb05
Muhammad Ragiel Prastyo <156585050+zYoozyo@users.noreply.github.com>
2025-06-09 21:40:26 +0700

    Delete S1IF-11-05_TuBes_Data Pengunjung Villa directory

--- S1IF-11-05_TuBes_Data Pengunjung Villa/src/Main.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.SwingUtilities;
-
-public class Main {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            new DataPengunjungFrame().setVisible(true);
-        });
-    }
-}
